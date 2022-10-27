2c330711a23d72c43771abfc87fe113aa13c9ef0
zvasib <zvasib@ya.ru>
2022-10-28 03:53:19 +0700

    Add eslint & prettier, husky, lint-stage

--- src/components/todo-app/todo-app.js
+++ src/components/todo-app/todo-app.js
@@ -17,6 +17,8 @@ export default class TodoApp extends Component {
   addTask = (text) => {
     const newTask = this.createTask(text);
 
+    console.log(newTask);
+
     this.setState(({ tasksData }) => {
       const newArray = [...tasksData, newTask];
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 import TodoApp from './components/todo-app';
 import './index.css';
 
-dasmdasdmas;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
