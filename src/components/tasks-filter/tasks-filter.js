976586240ae1d965f88ee713a5beca330e3328d5
zvasib <zvasib@ya.ru>
2022-12-14 00:48:48 +0700

    TasksFilter to hooks

--- src/components/tasks-filter/tasks-filter.js
+++ src/components/tasks-filter/tasks-filter.js
@@ -1,60 +1,55 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './tasks-filter.css';
 
-export default class TasksFilter extends Component {
-  static propTypes = {
-    currentFilter: PropTypes.oneOf(['all', 'active', 'completed']),
-    setFilter: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { currentFilter, setFilter } = this.props;
-
-    return (
-      <ul className="filters">
-        <li>
-          <label className={currentFilter === 'all' ? 'selected' : ''}>
-            <input
-              type="radio"
-              value="all"
-              name="filter"
-              onChange={() => {
-                setFilter('all');
-              }}
-            />
-            All
-          </label>
-        </li>
-        <li>
-          <label className={currentFilter === 'active' ? 'selected' : ''}>
-            <input
-              type="radio"
-              value="active"
-              name="filter"
-              onChange={() => {
-                setFilter('active');
-              }}
-            />
-            Active
-          </label>
-        </li>
-        <li>
-          <label className={currentFilter === 'completed' ? 'selected' : ''}>
-            <input
-              type="radio"
-              value="completed"
-              name="filter"
-              onChange={() => {
-                setFilter('completed');
-              }}
-            />
-            Completed
-          </label>
-        </li>
-      </ul>
-    );
-  }
+export default function TasksFilter({ currentFilter, setFilter }) {
+  return (
+    <ul className="filters">
+      <li>
+        <label className={currentFilter === 'all' ? 'selected' : ''}>
+          <input
+            type="radio"
+            value="all"
+            name="filter"
+            onChange={() => {
+              setFilter('all');
+            }}
+          />
+          All
+        </label>
+      </li>
+      <li>
+        <label className={currentFilter === 'active' ? 'selected' : ''}>
+          <input
+            type="radio"
+            value="active"
+            name="filter"
+            onChange={() => {
+              setFilter('active');
+            }}
+          />
+          Active
+        </label>
+      </li>
+      <li>
+        <label className={currentFilter === 'completed' ? 'selected' : ''}>
+          <input
+            type="radio"
+            value="completed"
+            name="filter"
+            onChange={() => {
+              setFilter('completed');
+            }}
+          />
+          Completed
+        </label>
+      </li>
+    </ul>
+  );
 }
+
+TasksFilter.propTypes = {
+  currentFilter: PropTypes.oneOf(['all', 'active', 'completed']),
+  setFilter: PropTypes.func.isRequired,
+};
