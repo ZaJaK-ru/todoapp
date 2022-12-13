44bbf4cf010c422a2586ce3fce8d0a2a1841af96
zvasib <zvasib@ya.ru>
2022-12-14 00:51:17 +0700

    Footer to hooks

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,33 +1,28 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import TasksFilter from '../tasks-filter';
 import './footer.css';
 
-export default class Footer extends Component {
-  static defaultProps = {
-    itemLeft: 0,
-    currentFilter: 'all',
-  };
-
-  static propTypes = {
-    itemLeft: PropTypes.number,
-    clearCompleted: PropTypes.func.isRequired,
-    setFilter: PropTypes.func.isRequired,
-    currentFilter: PropTypes.oneOf(['all', 'active', 'completed']),
-  };
-
-  render() {
-    const { itemLeft, setFilter, currentFilter, clearCompleted } = this.props;
-
-    return (
-      <footer className="footer">
-        <span className="todo-count">{itemLeft} items left </span>
-        <TasksFilter currentFilter={currentFilter} setFilter={setFilter} />
-        <button className="clear-completed" onClick={clearCompleted}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
+export default function Footer({ itemLeft, setFilter, currentFilter, clearCompleted }) {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{itemLeft} items left </span>
+      <TasksFilter currentFilter={currentFilter} setFilter={setFilter} />
+      <button className="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
 }
+
+Footer.defaultProps = {
+  itemLeft: 0,
+  currentFilter: 'all',
+};
+
+Footer.propTypes = {
+  itemLeft: PropTypes.number,
+  clearCompleted: PropTypes.func.isRequired,
+  setFilter: PropTypes.func.isRequired,
+  currentFilter: PropTypes.oneOf(['all', 'active', 'completed']),
+};
