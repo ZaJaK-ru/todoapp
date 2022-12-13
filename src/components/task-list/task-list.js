07aa119b493f7ea909ac453ace5f1cb6db6547e9
zvasib <zvasib@ya.ru>
2022-12-14 00:29:49 +0700

    TaskList to hooks

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Task from '../task';
@@ -6,56 +5,53 @@ import EditTask from '../edit-task';
 
 import './task-list.css';
 
-export default class TaskList extends Component {
-  static propTypes = {
-    tasks: PropTypes.arrayOf(
-      PropTypes.shape({
-        description: PropTypes.string.isRequired,
-        created: PropTypes.instanceOf(Date).isRequired,
-        completed: PropTypes.bool.isRequired,
-        editing: PropTypes.bool.isRequired,
-        id: PropTypes.number.isRequired,
-      })
-    ),
-    onEditing: PropTypes.func.isRequired,
-    onDeleted: PropTypes.func.isRequired,
-    onChecked: PropTypes.func.isRequired,
-    saveEdit: PropTypes.func.isRequired,
-  };
+export default function TaskList({ tasks, onEditing, onDeleted, onChecked, saveEdit, timerStartStop }) {
+  const elements = tasks.map((item) => {
+    const { id, completed, editing, ...data } = item;
+    let liStyle = '';
 
-  render() {
-    const { tasks, onEditing, onDeleted, onChecked, saveEdit, timerStartStop } = this.props;
+    if (editing) liStyle += ' editing';
+    if (completed) liStyle += ' completed';
 
-    const elements = tasks.map((item) => {
-      const { id, completed, editing, ...data } = item;
-      let liStyle = '';
+    return (
+      <li key={id} className={liStyle}>
+        <Task
+          {...data}
+          onDeleted={() => {
+            onDeleted(id);
+          }}
+          onEditing={() => {
+            onEditing(id);
+          }}
+          onChecked={() => {
+            onChecked(id);
+          }}
+          onStartStop={() => {
+            timerStartStop(id);
+          }}
+          completed={completed}
+        />
+        {editing && <EditTask description={item.description} id={id} saveEdit={saveEdit} />}
+      </li>
+    );
+  });
 
-      if (editing) liStyle += ' editing';
-      if (completed) liStyle += ' completed';
-
-      return (
-        <li key={id} className={liStyle}>
-          <Task
-            {...data}
-            onDeleted={() => {
-              onDeleted(id);
-            }}
-            onEditing={() => {
-              onEditing(id);
-            }}
-            onChecked={() => {
-              onChecked(id);
-            }}
-            onStartStop={() => {
-              timerStartStop(id);
-            }}
-            completed={completed}
-          />
-          {editing && <EditTask description={item.description} id={id} saveEdit={saveEdit} />}
-        </li>
-      );
-    });
-
-    return <ul className="todo-list">{elements}</ul>;
-  }
+  return <ul className="todo-list">{elements}</ul>;
 }
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      created: PropTypes.instanceOf(Date).isRequired,
+      completed: PropTypes.bool.isRequired,
+      editing: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+  onEditing: PropTypes.func.isRequired,
+  onDeleted: PropTypes.func.isRequired,
+  onChecked: PropTypes.func.isRequired,
+  saveEdit: PropTypes.func.isRequired,
+  timerStartStop: PropTypes.func.isRequired,
+};
